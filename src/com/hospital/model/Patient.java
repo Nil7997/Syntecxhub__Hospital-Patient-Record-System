3d617dc584cb0c35b40ffe601c5da560fdb906ae
Nil7997 <nileshm7997@gmail.com>
2026-02-12 13:41:48 +0530

    Update Patient.java

--- src/com/hospital/model/Patient.java
+++ src/com/hospital/model/Patient.java
@@ -1,7 +1,7 @@
 package com.hospital.model;
 
-public class Patient {
-
+public class Patient 
+{
     private int patientId;
     private String name;
     private int age;
@@ -11,7 +11,8 @@ public class Patient {
 
     public Patient() {}
 
-    public Patient(String name, int age, String gender, String contact, String medicalHistory) {
+    public Patient(String name, int age, String gender, String contact, String medicalHistory) 
+    {
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -21,21 +22,33 @@ public class Patient {
 
     // Getters and Setters
 
-    public int getPatientId() { return patientId; }
-    public void setPatientId(int patientId) { this.patientId = patientId; }
+    public int getPatientId() 
+    { return patientId; }
+    public void setPatientId(int patientId) 
+    { this.patientId = patientId; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() 
+    { return name; }
+    public void setName(String name) 
+    { this.name = name; }
 
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
+    public int getAge() 
+    { return age; }
+    public void setAge(int age) 
+    { this.age = age; }
 
-    public String getGender() { return gender; }
-    public void setGender(String gender) { this.gender = gender; }
+    public String getGender() 
+    { return gender; }
+    public void setGender(String gender) 
+    { this.gender = gender; }
 
-    public String getContact() { return contact; }
-    public void setContact(String contact) { this.contact = contact; }
+    public String getContact() 
+    { return contact; }
+    public void setContact(String contact) 
+    { this.contact = contact; }
 
-    public String getMedicalHistory() { return medicalHistory; }
-    public void setMedicalHistory(String medicalHistory) { this.medicalHistory = medicalHistory; }
+    public String getMedicalHistory()
+    { return medicalHistory; }
+    public void setMedicalHistory(String medicalHistory) 
+    { this.medicalHistory = medicalHistory; }
 }
