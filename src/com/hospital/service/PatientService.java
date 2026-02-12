a0ed2f0de3d0ea8a9c03b49f897203812029a35a
Nil7997 <nileshm7997@gmail.com>
2026-02-12 13:43:18 +0530

    Refactor PatientService methods for consistency

--- src/com/hospital/service/PatientService.java
+++ src/com/hospital/service/PatientService.java
@@ -7,36 +7,42 @@ import com.hospital.dao.PatientDAO;
 import com.hospital.dao.PatientDAOImpl;
 import com.hospital.model.Patient;
 
-public class PatientService {
-
+public class PatientService 
+{
 	private PatientDAO dao = new PatientDAOImpl();
 
-	public boolean registerPatient(Patient patient) {
-
-		if (patient.getAge() <= 0) {
+	public boolean registerPatient(Patient patient) 
+	{
+		if (patient.getAge() <= 0) 
+		{
 			throw new IllegalArgumentException("Invalid Age");
 		}
 
-		if (!patient.getContact().matches("[0-9]{10}")) {
+		if (!patient.getContact().matches("[0-9]{10}")) 
+		{
 			throw new IllegalArgumentException("Invalid Contact Number");
 		}
 
 		return dao.addPatient(patient);
 	}
 
-	public boolean updateHistory(int id, String history) {
+	public boolean updateHistory(int id, String history) 
+	{
 		return dao.updateMedicalHistory(id, history);
 	}
 
-	public Patient findPatient(int id) {
+	public Patient findPatient(int id) 
+	{
 		return dao.getPatientById(id);
 	}
 
-	public List<Patient> listPatients() {
+	public List<Patient> listPatients() 
+	{
 		return dao.getAllPatients();
 	}
 
-	public boolean removePatient(int id) {
+	public boolean removePatient(int id) 
+	{
 		return dao.deletePatient(id);
 	}
 }
